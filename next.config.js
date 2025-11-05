914ff9aab59bd17ecb36bcd2f86221a4645b574c
badaru nuratullah <badarunuratullaho@gmail.com>
2025-11-05 10:12:51 +0100

    Remove @expo/next-adapter and fix Next.js config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,16 @@
-const { withExpo } = require('@expo/next-adapter');
 const path = require('path');
 
 /** @type {import('next').NextConfig} */
-module.exports = withExpo({
-  transpilePackages: [
-    'react-native',
-    'react-native-web',
-    'expo',
-    'expo-router',
-    '@expo/vector-icons',
-    '@react-navigation/native',
-    'react-native-reanimated',
-    'react-native-safe-area-context',
-    'react-native-gesture-handler',
-  ],
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      'react-native$': 'react-native-web',
       '@': path.resolve(__dirname),
     };
     return config;
   },
-});
+};
+
+module.exports = nextConfig;
